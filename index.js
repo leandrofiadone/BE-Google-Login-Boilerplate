c331272f1ef51906844cc79b97f9e45877d48b40
Leandro <leanfiadone@gmail.com>
2025-02-24 15:01:16 -0300

    correcion backend

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('dotenv').config();
-require('./models/dbConnect');
-const authRoutes = require('./routes/authRoutes');
-const PORT = process.env.PORT || 8080;
-
-app.use(cors());
+const express = require("express")
+const cors = require("cors")
+const app = express()
+require("dotenv").config()
+require("./models/dbConnect")
+const authRoutes = require("./routes/authRoutes")
+const PORT = process.env.PORT || 8080
 
 app.use(
   cors({
-
-    origin: ['https://be-google-login-boilerplate.onrender.com/', 'http://localhost:5173'],
+    origin: [
+      "https://be-google-login-boilerplate.onrender.com",
+      "http://localhost:5173"
+    ],
     credentials: true
   })
 )
-app.use('/auth/', authRoutes); // <- NEW LINE
 
-app.all('*', (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
-});
+app.use("/auth/", authRoutes)
+
+// Simplemente enviar una respuesta de error directamente sin usar AppError
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on the server`
+  })
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
+  console.log(`Server is running on ${PORT}`)
 })
